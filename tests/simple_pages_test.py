f90eb340157319b1432720b2a07190482a7a85b1
LaderBen <z874154748@sina.com>
2022-02-27 18:09:43 -0500

    changed test rule

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -13,14 +13,20 @@ def test_request_about(client):
     assert response.status_code == 200
     assert b"About Page" in response.data
 
-def test_request_page1(client):
+def test_request_git_page(client):
     """This makes the index page"""
-    response = client.get("/page1")
+    response = client.get("/git")
     assert response.status_code == 200
-    assert b"Page 1" in response.data
+    assert b"Git" in response.data
 
-def test_request_page2(client):
+def test_request_docker_page(client):
     """This makes the index page"""
-    response = client.get("/page2")
+    response = client.get("/docker")
     assert response.status_code == 200
-    assert b"Page 2" in response.data
+    assert b"Docker" in response.data
+
+def test_request_flask_page(client):
+    """This makes the index page"""
+    response = client.get("/flask")
+    assert response.status_code == 200
+    assert b"Flask" in response.data
