cb32d4315f95b213554af3d9ea39df094f2d678b
laderben <z874154748@sina.com>
2022-03-21 16:36:56 -0400

    add the operation class in calculator file, add realized the function.

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,4 +1,5 @@
 """ This is the Calculator Class"""
+from calculator.operations import Addition, Subtraction, Multiplication, Divition
 
 
 class Calculator:
@@ -7,12 +8,22 @@ class Calculator:
 
     def add(self, value_1):
         """ This is the add method"""
-        self.result = self.result + value_1
+        self.result = Addition.add(self.result, value_1)
         return self.result
 
     def subtract(self, value_1):
         """ This is the subtract method"""
-        self.result = self.result - value_1
+        self.result = Subtraction.subtract(self.result, value_1)
+        return self.result
+
+    def multiply(self, value_1, value_2):
+        """This is the multiply method"""
+        self.result = Multiplication.multiply(value_1, value_2)
+        return self.result
+
+    def divide(self, value_1, value_2):
+        """This is the divide method"""
+        self.result = Divition.divide(value_1, value_2)
         return self.result
 
     def get_result(self):
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -7,25 +7,30 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"Index Page" in response.data
 
+
 def test_request_git_page(client):
-    """This makes the index page"""
+    """This makes the git page"""
     response = client.get("/git")
     assert response.status_code == 200
     assert b"Git" in response.data
 
+
 def test_request_docker_page(client):
-    """This makes the index page"""
+    """This makes the docker page"""
     response = client.get("/docker")
     assert response.status_code == 200
     assert b"Docker" in response.data
 
+
 def test_request_flask_page(client):
-    """This makes the index page"""
+    """This makes the flask page"""
     response = client.get("/flask")
     assert response.status_code == 200
     assert b"Flask" in response.data
 
+
 def test_request_cicd_page(client):
-        response = client.get("/CICD")
-        assert response.status_code == 200
-        assert b"CI/CD" in response.data
+    """This makes the CICD page"""
+    response = client.get("/CICD")
+    assert response.status_code == 200
+    assert b"CI/CD" in response.data
